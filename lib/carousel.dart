828c8b2e345a2de83963928affad2948eca21002
Kamolnadda Dansuputra <kamolnaddadansuputra@Kamolnaddas-MacBook-Pro.local>
2022-11-02 15:30:39 +0700

    add swipe

--- lib/carousel.dart
+++ lib/carousel.dart
@@ -75,6 +75,8 @@ class _CarouselState extends State<Carousel> {
 
   @override
   Widget build(BuildContext context) {
+    String? swipeDirection;
+    String? swipeDirectionScroll;
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.width * 0.75;
 
@@ -88,143 +90,208 @@ class _CarouselState extends State<Carousel> {
             )
           : Column(
               children: [
-                Stack(
-                  children: [
-                    ..._animatedImage,
-                    SizedBox(
-                      width: width,
-                      height: height,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () {
-                              if (_index <= 0) {
-                                return;
-                              }
-                              setState(() {
-                                _index--;
-                                _animatedImage = [];
-                                addimageList();
-                                if (_index > _scrollIndex + 3 ||
-                                    _index < _scrollIndex) {
-                                  if (_index <= widget.images.length - 4) {
-                                    _scrollIndex = _index;
-                                  } else {
-                                    _scrollIndex = widget.images.length - 4;
-                                  }
-                                  _scroll();
+                GestureDetector(
+                  onHorizontalDragEnd: (detail) {
+                    if (detail.primaryVelocity == null) {
+                      return;
+                    } else if (detail.primaryVelocity! < 0) {
+                      if (_index >= widget.images.length - 1) {
+                        return;
+                      }
+                      setState(() {
+                        _index++;
+                        _animatedImage = [];
+                        addimageList();
+                        if (_index > _scrollIndex + 3 ||
+                            _index < _scrollIndex) {
+                          if (_index <= widget.images.length - 4) {
+                            _scrollIndex = _index;
+                          } else {
+                            _scrollIndex = widget.images.length - 4;
+                          }
+                          _scroll();
+                        }
+                      });
+                    } else if (detail.primaryVelocity! > 0) {
+                      if (_index <= 0) {
+                        return;
+                      }
+                      setState(() {
+                        _index--;
+                        _animatedImage = [];
+                        addimageList();
+                        if (_index > _scrollIndex + 3 ||
+                            _index < _scrollIndex) {
+                          if (_index <= widget.images.length - 4) {
+                            _scrollIndex = _index;
+                          } else {
+                            _scrollIndex = widget.images.length - 4;
+                          }
+                          _scroll();
+                        }
+                      });
+                    }
+                  },
+                  child: Stack(
+                    children: [
+                      ..._animatedImage,
+                      SizedBox(
+                        width: width,
+                        height: height,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.arrow_back),
+                              onPressed: () {
+                                if (_index <= 0) {
+                                  return;
                                 }
-                              });
-                            },
-                          ),
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(Icons.arrow_forward),
-                            onPressed: () {
-                              if (_index >= widget.images.length - 1) {
-                                return;
-                              }
-                              setState(() {
-                                _index++;
-                                _animatedImage = [];
-                                addimageList();
-                                if (_index > _scrollIndex + 3 ||
-                                    _index < _scrollIndex) {
-                                  if (_index <= widget.images.length - 4) {
-                                    _scrollIndex = _index;
-                                  } else {
-                                    _scrollIndex = widget.images.length - 4;
+                                setState(() {
+                                  _index--;
+                                  _animatedImage = [];
+                                  addimageList();
+                                  if (_index > _scrollIndex + 3 ||
+                                      _index < _scrollIndex) {
+                                    if (_index <= widget.images.length - 4) {
+                                      _scrollIndex = _index;
+                                    } else {
+                                      _scrollIndex = widget.images.length - 4;
+                                    }
+                                    _scroll();
                                   }
-                                  _scroll();
+                                });
+                              },
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.arrow_forward),
+                              onPressed: () {
+                                if (_index >= widget.images.length - 1) {
+                                  return;
                                 }
-                              });
-                            },
-                          )
-                        ],
+                                setState(() {
+                                  _index++;
+                                  _animatedImage = [];
+                                  addimageList();
+                                  if (_index > _scrollIndex + 3 ||
+                                      _index < _scrollIndex) {
+                                    if (_index <= widget.images.length - 4) {
+                                      _scrollIndex = _index;
+                                    } else {
+                                      _scrollIndex = widget.images.length - 4;
+                                    }
+                                    _scroll();
+                                  }
+                                });
+                              },
+                            )
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
-                Stack(
-                  children: [
-                    SizedBox(
-                      height: height * 0.2,
-                      child: ListView.separated(
-                        itemCount: widget.images.length,
-                        separatorBuilder: (context, i) => const SizedBox(
-                          width: 10,
-                        ),
-                        scrollDirection: Axis.horizontal,
-                        physics: const NeverScrollableScrollPhysics(),
-                        controller: _controller,
-                        itemBuilder: ((context, i) => AutoScrollTag(
-                              key: ValueKey(i),
-                              controller: _controller,
-                              index: i,
-                              child: InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    _index = i;
-                                    _animatedImage = [];
-                                    addimageList();
-                                  });
-                                },
-                                child: Container(
-                                  width: (width - 30) / 4,
-                                  height: height * 0.2,
-                                  decoration: BoxDecoration(
-                                      image: DecorationImage(
-                                          image: NetworkImage(widget.images[i]),
-                                          fit: BoxFit.cover)),
-                                ),
-                              ),
-                            )),
-                      ),
-                    ),
-                    SizedBox(
-                      width: width,
-                      height: height * 0.2,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(Icons.arrow_back),
-                            onPressed: () {
-                              setState(() {
-                                if (_scrollIndex > 0) {
-                                  _scrollIndex--;
-                                  _scroll();
-                                }
-                              });
-                            },
+                GestureDetector(
+                  onHorizontalDragEnd: (detail) {
+                    if (detail.primaryVelocity == null) {
+                      return;
+                    } else if (detail.primaryVelocity! < 0) {
+                      if (_scrollIndex <= widget.images.length - 5) {
+                        setState(() {
+                          _scrollIndex++;
+                          _scroll();
+                        });
+                      }
+                    } else if (detail.primaryVelocity! > 0) {
+                      setState(() {
+                        if (_scrollIndex > 0) {
+                          _scrollIndex--;
+                          _scroll();
+                        }
+                      });
+                    }
+                  },
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        height: height * 0.2,
+                        child: ListView.separated(
+                          itemCount: widget.images.length,
+                          separatorBuilder: (context, i) => const SizedBox(
+                            width: 10,
                           ),
-                          IconButton(
-                            padding: EdgeInsets.zero,
-                            constraints: const BoxConstraints(),
-                            icon: const Icon(Icons.arrow_forward),
-                            onPressed: () {
-                              if (_scrollIndex <= widget.images.length - 5) {
-                                setState(() {
-                                  _scrollIndex++;
-                                  _scroll();
-                                });
-                              }
-                            },
-                          )
-                        ],
+                          scrollDirection: Axis.horizontal,
+                          physics: const NeverScrollableScrollPhysics(),
+                          controller: _controller,
+                          itemBuilder: ((context, i) => AutoScrollTag(
+                                key: ValueKey(i),
+                                controller: _controller,
+                                index: i,
+                                child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      _index = i;
+                                      _animatedImage = [];
+                                      addimageList();
+                                    });
+                                  },
+                                  child: Container(
+                                    width: (width - 30) / 4,
+                                    height: height * 0.2,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image:
+                                                NetworkImage(widget.images[i]),
+                                            fit: BoxFit.cover)),
+                                  ),
+                                ),
+                              )),
+                        ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: width,
+                        height: height * 0.2,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.arrow_back),
+                              onPressed: () {
+                                setState(() {
+                                  if (_scrollIndex > 0) {
+                                    _scrollIndex--;
+                                    _scroll();
+                                  }
+                                });
+                              },
+                            ),
+                            IconButton(
+                              padding: EdgeInsets.zero,
+                              constraints: const BoxConstraints(),
+                              icon: const Icon(Icons.arrow_forward),
+                              onPressed: () {
+                                if (_scrollIndex <= widget.images.length - 5) {
+                                  setState(() {
+                                    _scrollIndex++;
+                                    _scroll();
+                                  });
+                                }
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
